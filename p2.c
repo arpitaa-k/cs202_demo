5aa8d5448d09aa5e8c5db6647c6b949c7ec4751f
arpitaa-k <23110045@iitgn.ac.in>
2025-08-04 14:32:45 +0530

    done

--- p2.c
+++ p2.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
-
-int main(){
-    int n = 5;
-    for(int i=0;i<n;i++){
-        printf("%d", i);
+void copyMemory(char* dest, const char* src, int n){
+    for(int i =0;i<n;i++){
+        dest[i] = src[i];
     }
+}
+int main(){
+    char buffer[256];
+    const char* msg = "Hello World";
+
+    int numBytes = sizeof(msg);
+    copyMemory(buffer, msg, numBytes);
+    printf("Copied message: %s\n", buffer);
+    return 0;
 }
